db732657592b94ebe439138a15f2efbb777f1a05
Bedirhan233 <bedirhanko73@gmail.com>
2023-09-15 14:37:02 +0200

    hej
    d d d

--- Assets/Script/TestMedCol.cs
+++ Assets/Script/TestMedCol.cs
@@ -4,27 +4,7 @@ using UnityEngine;
 
 public class TestMedCol : MonoBehaviour
 {
-    Rigidbody2D rigidbody2;
+    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody2 = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    // Use this for 2D collision detection
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.name == "DownBorder")
-        {
-            Debug.Log("Collision with DownBorder");
-            Destroy(gameObject);
-        }
-    }
+    // hej hej hej hej hjej eqknakjlnjaslkdmjsd
 }
